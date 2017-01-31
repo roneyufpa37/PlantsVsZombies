75c403fa7b8d2cc9fb901685963822501f44de8b
FACOMP <FACOMP@LAB3-PC04>
2017-01-31 14:46:36 -0300

    .

--- PlantsVsZombies/Personagem.cpp
+++ PlantsVsZombies/Personagem.cpp
@@ -1,20 +1,56 @@
-#include"Personagem.h"
+#include "personagem.h"
 
-Personagem::Personagem(){
-    vivo = "talvez";
-    valor = 0;
+Personagem::Personagem()
+{
+    nomePersonagem = "nenhum";
+    vivo = false;
 }
 
-Personagem(Personagem &namepersonagem){
-    this.vivo= Personagem.namepersonagem;
-    this.valor = Personagem.valor;
+Personagem::Personagem(const Personagem &nomePersonagem){
+	this->nomePersonagem = nomePersonagem;
+	this->vivo=true;
 }
-void Personagem::addpersonagens(const string &namepersonagem){
-    string *aux = new string[vivo];
-    for (int = 0; i <vivo;i++)
-        aux[i] = namepersonagem
+Personagem::Personagem(const Personagem &personagem)
+{
+	this->nomePersonagem = personagem.nomePersonagem;
+	this->vivo = personagem.morto;
+}
+void Personagem::addPersonagens(const string &nome)
+{
+	string *aux = new string[qtd];
+	
+	for (int i=0; i < qtd; i++)
+		aux[i] = nomePersonagem[i];
+		delete [] nomePersonagem;
+		
+		nomePersonagem = new string[++qtd];
+		
+    for (int i = 0; i < qtd-1; i++)
+		nomePersonagem[i] = aux[i];		
+		nomePersonagem[qtd-1] = nome;
+		delete [] aux;
 }
 
-Personagem:~Personagem(){
-    
+void anda()
+{
+	cout << "Personagem morreu?(True or False)" << endl;
+	cin << vivo << endl;
+      if (vivo==true)
+	    cout << "Personagem continua no jogo" << endl;
+	  else
+	    cout << "Personagem saiu do jogo" << endl;
+}
+void preco(){
+	cout << "Quantos personagens tem?"
+	cin << qtd << endl;
+	if (qtd>0 && qtd<=25)
+		cout << "Poucos Personagens" << endl;
+		else if (qtd>25 && qtd<50)
+	    cout << "Muitos Personagens" << endl;
+		else
+	    cout << "Quantidade Invalida" << endl;
+}
+Personagem::~Personagem()
+{
+	
 }
--- PlantsVsZombies/Personagem.h
+++ PlantsVsZombies/Personagem.h
@@ -1,18 +1,30 @@
+//Nova Classe chamada Personagem
+//1 construtor default, 1 construtor de copia e 1 outro construtor para a classe
+// Com dois atributos e dois metodos
+//Um metodo para adicionar personagens, seguindo o exemplo proposto nos slides da ultima aula
+// e mais um atributo ponteiro
 #ifndef PERSONAGEM_H
 #define PERSONAGEM_H
+#include <stdio.h>
 #include <string>
-
-class Personagem 
+aa
+using std::string;
+using std::cout;
+using std::endl;
+using std::cin;
+class Personagem
 {
-public :
-    Personagem();
-    Personagem(Personagem &);
-    bool vivo();
-    int valor();
-    void addpersonagens( const string &, const int);
-    
+public:
+	  Personagem();
+	  Personagem(const Personagem &, int);
+	  Personagem(const Personagem &);
+	  void anda();
+	  void preco();
+	  void addPersonagens(const string &);
+
 private:
-    string namepersonagem;
-    bool vivo;
+      int qtd;
+	  bool vivo;
+      string *nomePersonagem;
 };
-#endif
+
